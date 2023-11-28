dc2798c0b55c7a44724131c68fc8b7e8f5be2c11
bobbyslug <he.chr@northeastern.edu>
2023-11-28 04:51:12 -0500

    batch normalization

--- environment/agent.py
+++ environment/agent.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
 import torch
 import random
 import os
@@ -15,8 +16,10 @@ class AgentNet(nn.Module):
         # Input size based on number of outputs from Observation
         # This can be obtained from Observation.configured_size()
         self.fc1 = nn.Linear(Observation.configured_size(), 32)
+        self.batch_norm1 = nn.BatchNorm1d(32, affine=False, track_running_stats=False)
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(32, 20)
+        self.batch_norm2 = nn.BatchNorm1d(20, affine=False, track_running_stats=False)
         self.relu2 = nn.ReLU()
         # Output size based on number of actions, couldn't find constant of how many actions exist, probably don't need one, just update if more actions
         self.fc3 = nn.Linear(20, 11)
@@ -24,10 +27,15 @@ class AgentNet(nn.Module):
 
     def forward(self, x):
         x = self.fc1(x)
+        x = self.batch_norm1(x.view(x.size(0), -1))
+        x = x.view(x.size(0), -1)
         x = self.relu(x)
+        print(x, "x shape")
         x = self.fc2(x)
+        x = self.batch_norm2(x.view(x.size(0), -1))
         x = self.relu2(x)
         x = self.fc3(x)
+        x = F.softmax(x, dim=-1)
         return x
     
 class Agent:
@@ -51,7 +59,7 @@ class Agent:
     def store_experience(self, experience):
         self.replay_buffer.append(experience)
 
-    def learn(self, lr: float = 0.0001, epochs: int = 100, replay_buffer_size: int = 1000, batch_size: int = 10):
+    def learn(self, lr: float = 0.0001, epochs: int = 100, replay_buffer_size: int = 1000, batch_size: int = 32):
         optimizer = optim.Adam(self.net.parameters(), lr=lr) # learning rate schedule?
         criterion = nn.CrossEntropyLoss()
 
@@ -65,7 +73,7 @@ class Agent:
             game_finished = False
             observation, _ = self.env.reset()
             while not game_finished:
-                action = self.select_action(observation, exploration_rate=1.0 - epoch / epochs)
+                action = self.select_action(observation, exploration_rate=0.1)
 
                 next_observation, reward, terminal, truncated, info = self.env.step(action)
                 game_finished = terminal or truncated
@@ -109,16 +117,27 @@ class Agent:
 
 
     def select_action(self, observation: Observation, exploration_rate: float = 0.1):
-        # forward pass for action probabilities
         flattened_data = torch.tensor(observation.flattened_data, dtype=torch.float32)
-        # print("Flattened Data:", flattened_data)
-        # print("Network Output:", self.net(flattened_data))
-        # print("Max Value:", torch.max(self.net(flattened_data)))
-        action_prob = torch.exp(torch.log_softmax(self.net(flattened_data) - torch.max(self.net(flattened_data)), dim=-1) + 1e-8)
-        action_prob = torch.clamp(action_prob, min=1e-8)
-        # or pick the action with the highest probability
-        # action = torch.argmax(action_prob).item()
-        action = random.randint(0, 10)
+        action_logits = self.net(flattened_data)
+        
+        # Check for invalid values in action_logits
+        if torch.isnan(action_logits).any() or torch.isinf(action_logits).any():
+            print("Invalid values detected in action_logits.")
+            return 0  # Or handle this case appropriately
+
+        # Apply softmax to get probabilities
+        action_prob = torch.exp(torch.log_softmax(action_logits, dim=-1) + 1e-8)
+        
+        # Check for invalid values in action_prob
+        if torch.isnan(action_prob).any() or torch.isinf(action_prob).any():
+            print("Invalid values detected in action_prob.")
+            return 0  # Or handle this case appropriately
+
+        if random.random() < exploration_rate:
+            action = random.randint(0, 10)
+        else:
+            print(action_prob)
+            action = torch.multinomial(action_prob, 1).item()
 
         return action
 
@@ -130,16 +149,13 @@ class Agent:
         observation = game.get_observation()
         while not game_finished:
             # forward pass for action probabilities
-            flattened_data = torch.tensor(observation.flattened_data, dtype=torch.float32)
-            action_prob = torch.softmax(self.net(flattened_data), dim=-1)
-
             # sample an action from the probability distribution
-            action = torch.multinomial(action_prob, 1).item()
+            action = self.select_action(observation)
             # or pick the action with the highest probability
             # action = torch.argmax(action_prob).item()
 
             # take the decided action
-            observation, reward, game_finished, truncated, info  = game.step(action)
+            observation, reward, game_finished, _, _  = game.step(action)
             total_reward += reward
         
         return total_reward
